be99a373740fd7783847fe0dc7bc548641bd4371
Dan Guido <dan@trailofbits.com>
2014-05-21 17:08:40 -0400

    latest version

--- parslet/json2/lib/json_parser.rb
+++ parslet/json2/lib/json_parser.rb
@@ -1,1 +1,21 @@
-../../level1/lib/json_parser.rb
+require 'parslet'
+
+class JSONParser < Parslet::Parser
+
+  rule(:spaces)  { match['\s'].repeat(1) }
+  rule(:spaces?) { spaces.maybe }
+  rule(:digit)   { match['0-9'] }
+  rule(:char)    { match["^\""] }
+  rule(:comma)   { spaces? >> str(',') >> spaces? }
+
+  rule(:null)       { }
+  rule(:boolean)    { }
+  rule(:integer)    { }
+  rule(:string)     { }
+  rule(:array)      { }
+  rule(:hash_table) { }
+  rule(:node)       { string | integer | array | hash_table | boolean | null }
+
+  root :array
+
+end
